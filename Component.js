f29d9bda7c109679621db99ab7900395adedebeb
ivlgit2 <ivlgit2@gmail.com>
2024-02-14 12:10:27 +0000

    initial

---
+++ Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("bri.dbk_air_customization_hsnwise.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
